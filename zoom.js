e32c192c30773be22a6e15e01c956cb1f3af8cae
lilian <lilian.kigunda@student.moringaschool.com>
2021-10-13 09:47:35 +0300

    add customers logos

--- zoom.js
+++ zoom.js
@@ -13,3 +13,25 @@ new google.maps.Map(document.getElementById("map"), {
     },
   });
   
+
+  // var responsiveSlider = function() {
+
+  //   var slider = document.getElementById("slider");
+  //   var sliderWidth = slider.offsetWidth;
+  //   var slideList = document.getElementById("slideWrap");
+  //   var count = 1;
+  //   var items = slideList.querySelectorAll("li").length;
+  //   var prev = document.getElementById("prev");
+  //   var next = document.getElementById("next");
+    
+  //   window.addEventListener('resize', function() {
+  //     sliderWidth = slider.offsetWidth;
+  //   });
+  //   setInterval(function() {
+  //     nextSlide()
+  //   }, 8000);
+    
+  //   };
+  //   window.onload = function() {
+  //     responsiveSlider();  
+  //     }
